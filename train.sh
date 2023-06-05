5e334fc9d07d9e61a455a86039b4c642755d41d2
Rebecca Stone <ysbecca@gmail.com>
2023-06-05 12:02:34 +0100

    gitignore

---
+++ train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# test bayesian train no de-biasing
+
+
+
+python main_polypGen.py \
+	--cycle_length 2 \
+	--cycles 1 \
+	--model "deeplabv3plus_resnet50" \
+	--lr 0.1
+
+
+
+
+
+
+
+
