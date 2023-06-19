63b4c9282299c871f5bfe075c459fd94a7d40e72
Rebecca Stone <rsstone@login2.bede.dur.ac.uk>
2023-06-19 12:56:59 +0100

    few tweaks...

--- train.sh
+++ train.sh
@@ -4,13 +4,15 @@
 # test bayesian train no de-biasing
 
 
+export WANDB_DIR="/users/rsstone/projects_sym/rsstone/BayPolypGen-Benchmark/"
 
 python main_polypGen.py \
 	--cycle_length 3 \
 	--cycles 1 \
-	--models_per_cycle 1 \
+	--models_per_cycle 2 \
+	--batch_size 3 \
 	--model "deeplabv3plus_resnet50" \
-	--root "/usr/not-backed-up/BayPolypGen-Benchmark/datasets/" \
-	--lr 0.1
-	# --dev_run True  # doesn't create wandb run or save checkpoints
+	--root "/users/rsstone/projects_sym/rsstone/BayPolypGen-Benchmark/" \
+	--lr 0.1 \
+	--dev_run True  # doesn't create wandb run or save checkpoints
 
